66e0f2b607ac704c0512e41ad22d5569125d57eb
WillMerges <wdmerges@gmail.com>
2020-08-25 21:49:07 -0400

    Message Logger updates

--- lib/dls/src/MsgLogger.cpp
+++ lib/dls/src/MsgLogger.cpp
@@ -1,35 +1,32 @@
 #include "dls.h"
-#include <mqueue.h>
 
 using namespace dls;
 
-MsgLogger::MsgLogger(): class_name(""), func_name(""){}
+MsgLogger::MsgLogger(): class_name(""), func_name(""), open(false) {}
 
-MsgLogger::MsgLogger(std::string class_name, std::string func_name) {
+MsgLogger::MsgLogger(std::string class_name, std::string func_name): open(false) {
     this->class_name = class_name;
     this->func_name = func_name;
 }
 
-MsgLogger::MsgLogger(std::string func_name): class_name("") {
+MsgLogger::MsgLogger(std::string func_name): class_name(""), open(false) {
     this->func_name = func_name;
 }
 
-RetType MsgLogger::log_message(std::string msg) { //TODO maybe remove this from the class and make it a normal function so the telemtry data logger can use it
-    // TODO add timestamp? (maybe let the writing process do this)
-    std::string new_msg = class_name + ", " + func_name + ": " + msg;
-
-    mqd_t mq;
-    mq = mq_open(MQUEUE_NAME, O_WRONLY);
+RetType MsgLogger::Open() {
+    if(open) {
+        return SUCCESS;
+    }
+    mq = mq_open(MSG_MQUEUE_NAME, O_WRONLY);
     if((mqd_t)-1 == mq) {
         // no sense in logging this because it is the logger!
         return FAILURE;
     }
+    open = true;
+    return SUCCESS;
+}
 
-    if(0 > mq_send(mq, msg.c_str(), msg.size(), 0)) {
-        // no sense in logging this because it is the logger!
-        return FAILURE;
-    }
-
+RetType MsgLogger::Close() {
     if((mqd_t)-1 == mq_close(mq)) {
         std::string err = "Failed to close mqueue!";
         for(int i = 0; i<5; i++) { // try to send error msg 5 times
@@ -38,5 +35,18 @@ RetType MsgLogger::log_message(std::string msg) { //TODO maybe remove this from 
             }
         }
     }
+    open = false;
+    return FAILURE;
+}
+
+RetType MsgLogger::log_message(std::string msg) {
+    // TODO add timestamp? (maybe let the writing process do this)
+    std::string new_msg = class_name + ", " + func_name + ": " + msg;
+
+    if(0 > mq_send(mq, msg.c_str(), msg.size(), 0)) {
+        // no sense in logging this because it is the logger!
+        return FAILURE;
+    }
+
     return SUCCESS;
 }
