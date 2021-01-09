5c3ed59d2d45c2bd6851155adb7a71f9a4844d7b
WillMerges <wdmerges@gmail.com>
2021-01-08 20:11:43 -0500

    updated TODO

--- include/lib/nm/nm.h
+++ include/lib/nm/nm.h
@@ -13,17 +13,18 @@ namespace nm {
 
     static const size_t MAX_Q_SIZE = 2048;
 
-    // handles the physical sending and receiving of messages over the network
-    // should only ever have one instance per device
+    // checks an mqueue of name /[device_name from VCM] for messages to send over UDP
+    // checks the UDP socket for incoming messages and writes them to shared memory
     class NetworkManager {
     public:
         NetworkManager(vcm::VCM* vcm);
         ~NetworkManager();
         RetType Open();
         RetType Close(); // returns fail if anything goes wrong
-        //return FAILURE if anything goes wrong
+
+        //returns FAILURE if anything goes wrong
         RetType Iterate(); // send a message from the mqueue and then receive any messages available and place them into shmeme
-        // TODO consider adding priority to sending some messages (like deployment) and then check other messages last
+        // TODO consider adding priority to sending some messages (like deployment)
     private:
         mqd_t mq;
         std::string mqueue_name;
@@ -38,6 +39,7 @@ namespace nm {
 
     // allows a process to queue a message to send
     // can have many instances
+    // sends a message to an associated NetworkManager's mqueue
     class NetworkInterface {
     public:
         NetworkInterface(vcm::VCM* vcm);
