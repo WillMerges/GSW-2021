3e8504f58b61c0cb37425240213e8803d5f16d09
WillMerges <wdmerges@gmail.com>
2021-02-14 12:58:27 -0500

    convert lib updated

--- lib/convert/src/convert.cpp
+++ lib/convert/src/convert.cpp
@@ -1,8 +1,10 @@
 #include "lib/convert/convert.h"
+#include "lib/dls/dls.h"
 #include <stdint.h>
 #include <string.h>
 
 using namespace vcm;
+using namespace dls;
 
 char result[MAX_CONVERSION_SIZE];
 
@@ -12,9 +14,12 @@ char result[MAX_CONVERSION_SIZE];
 // NOTE: may be a problem if unsigned and signed ints aren't the same length
 //       but that would be weird to have them not the same length
 RetType convert::convert_str(VCM* vcm, measurement_info_t* measurement, const void* data, std::string* dst) {
+    MsgLogger logger("CONVERT", "convert_str");
+
     switch(measurement->type) {
         case INT_TYPE: {
             if(measurement->size > (sizeof(long int))) {
+                logger.log_message("Measurement size too great to convert to integer");
                 return FAILURE; // TODO add logging
             }
 
@@ -46,19 +51,27 @@ RetType convert::convert_str(VCM* vcm, measurement_info_t* measurement, const vo
 
         // TODO maybe we need a double type
         case FLOAT_TYPE: {
-            if(measurement->size > (sizeof(float))) {
-                return FAILURE; // TODO add logging
+            size_t convert_size = 0;
+            if(measurement->size == sizeof(float)) {
+                convert_size = sizeof(float);
+            } else if(measurement->size == sizeof(double)) {
+                convert_size = sizeof(double);
             }
 
-            unsigned char val[sizeof(float)]; // always assume it's the biggest (don't care about a few bytes)
-            memset(val, 0, sizeof(float));
+            if(convert_size == 0) {
+                logger.log_message("Unable to convert floating type to float or double");
+                return FAILURE;
+            }
+
+            unsigned char val[convert_size]; // always assume it's the biggest (don't care about a few bytes)
+            memset(val, 0, convert_size);
 
             size_t addr = (size_t)measurement->addr;
             const unsigned char* buff = (const unsigned char*)data;
 
             if(vcm->recv_endianness != vcm->sys_endianness) {
                 for(size_t i = 0; i < measurement->size; i++) {
-                    val[sizeof(float) - i - 1] = buff[addr + i];
+                    val[convert_size - i - 1] = buff[addr + i];
                 }
             } else {
                 for(size_t i = 0; i < measurement->size; i++) {
