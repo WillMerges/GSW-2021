ff9a6c562d49b81c42612b5533fd22beb67b3e93
WillMerges <wdmerges@gmail.com>
2021-04-06 11:13:45 -0400

    updated install scripts

--- app/InfluxDB/grafana/install.sh
+++ app/InfluxDB/grafana/install.sh
@@ -2,4 +2,6 @@ sudo apt-get install -y adduser libfontconfig1
 wget https://dl.grafana.com/oss/release/grafana_7.3.7_amd64.deb
 sudo dpkg -i grafana_7.3.7_amd64.deb
 
+rm grafana_7.3.7_amd64.deb
+
 sudo cp grafana.ini /etc/grafana/grafana.ini
