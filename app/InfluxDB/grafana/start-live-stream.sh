a89e8a4a51f5d34c8941f62d5ed9c9fd7d9ffaf2
Straviaz <joy.sangsrichan@gmail.com>
2021-11-06 11:55:58 -0400

    remove max file

--- app/InfluxDB/grafana/start-live-stream.sh
+++ app/InfluxDB/grafana/start-live-stream.sh
@@ -1,2 +1,2 @@
-sudo gst-launch-1.0 v4l2src device="/dev/video0" ! videoconvert ! clockoverlay ! x264enc tune=zerolatency ! mpegtsmux ! hlssink playlist-root=http://192.168.0.11:8080 location=video-segments/segment_%05d.ts target-duration=5 max-files=5 
+sudo gst-launch-1.0 v4l2src device="/dev/video0" ! videoconvert ! clockoverlay ! x264enc tune=zerolatency ! mpegtsmux ! hlssink playlist-root=http://192.168.0.11:8080 location=video-segments/segment_%05d.ts target-duration=5  
 
