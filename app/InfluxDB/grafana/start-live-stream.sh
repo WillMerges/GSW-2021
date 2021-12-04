dbb53d7ea3343fd1a412924e2b0dcda46347ec34
Straviaz <joy.sangsrichan@gmail.com>
2021-12-04 10:09:54 -0500

    Joy can't stand C anymore

--- app/InfluxDB/grafana/start-live-stream.sh
+++
@@ -1,2 +0,0 @@
-#sudo gst-launch-1.0 v4l2src device="/dev/video0" ! videoconvert ! clockoverlay ! x264enc tune=zerolatency ! mpegtsmux ! hlssink playlist-root=http://0.0.0.0:8080 location=segment_%05d.ts target-duration=1
-sudo gst-launch-1.0v4lsrc device="/dev/video0" ! videoconvert ! videoscale ! video/x-raw ! clockoverlay ! theoraenc ! oggmux ! tcpserversink host=0.0.0.0 port=8080
