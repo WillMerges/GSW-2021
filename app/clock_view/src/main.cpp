caae5a6f337101f179528425e303210bfa3fa3bf
Aaron Chan <aarchan108@gmail.com>
2022-08-11 23:06:32 -0400

    aaron moment

--- app/clock_view/src/main.cpp
+++ app/clock_view/src/main.cpp
@@ -29,8 +29,8 @@ int main() {
     }
 
     if(FAILURE == cl.open()) {
-        logger.log_message("Failed to attach clock");
-        printf("Failed to attach clock\n");
+        logger.log_message("Failed to open clock");
+        printf("Failed to open clock\n");
 
         return -1;
     }
